f87616d351259103c558a60d462bee00de24e2c3
Juan Pablo C. Mejia <jpablocardona@gmail.com>
2020-03-30 19:11:11 -0500

    first commit

---
+++ service-account.py
@@ -0,0 +1,20 @@
+# service-account.py
+
+from oauth2client.service_account import ServiceAccountCredentials
+
+# The scope for the OAuth2 request.
+SCOPE = 'https://www.googleapis.com/auth/analytics.readonly'
+
+# The location of the key file with the key data.
+KEY_FILEPATH = 'rebus-plugin-dev-ab972f792e6a.json'
+
+# Defines a method to get an access token from the ServiceAccount object.
+
+
+def get_access_token():
+    return ServiceAccountCredentials.from_json_keyfile_name(
+        KEY_FILEPATH, SCOPE).get_access_token().access_token
+
+
+token = get_access_token()
+print(token)
